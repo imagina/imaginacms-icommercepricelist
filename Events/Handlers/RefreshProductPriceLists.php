59c5fea1473d69cc1bd6eb3d6f63970bc53e7b0c
Omar Chunza <odchunzag.une@gmail.com>
2023-09-26 17:56:59 -0500

    Actualizacion de modulo IcommercePricelist a laravel 10.

--- Events/Handlers/RefreshProductPriceLists.php
+++ Events/Handlers/RefreshProductPriceLists.php
@@ -3,15 +3,12 @@
 namespace Modules\Icommercepricelist\Events\Handlers;
 
 //Support
-use Modules\Icommerce\Entities\Product;
 use Modules\Icommercepricelist\Entities\ProductList;
 
 class RefreshProductPriceLists
 {
-
     public function __construct()
     {
-
     }
 
     public function handle($event)
@@ -20,25 +17,22 @@ class RefreshProductPriceLists
       if(\Schema::hasTable('icommercepricelist__price_lists')){
 
         $product = $event->entity;
-        foreach($product->priceLists as $priceList){
-          if($priceList->criteria=="percentage"){
-            $value=($product->price*$priceList->value)/100;//Example value pricelist = 5 (Calculate 5%)
-            $price=$product->price;
-            if($priceList->operation_prefix=="-"){
-              $price-=$value;
-            }else{
-              $price+=$value;
+        foreach ($product->priceLists as $priceList) {
+            if ($priceList->criteria == 'percentage') {
+                $value = ($product->price * $priceList->value) / 100; //Example value pricelist = 5 (Calculate 5%)
+                $price = $product->price;
+                if ($priceList->operation_prefix == '-') {
+                    $price -= $value;
+                } else {
+                    $price += $value;
+                }
+                ProductList::where('id', $priceList->pivot->id)->update([
+                    'price' => $price,
+                ]);
             }
-            ProductList::where('id',$priceList->pivot->id)->update([
-              "price"=>$price
-            ]);
-          }
         }
         
       }
 
     }//  handle
-
-
-
 }
