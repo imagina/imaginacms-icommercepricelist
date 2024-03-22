ff5993ae3024d2ebfa9880b8a2d8c864c95bc416
NicolasMB <nmontealegre@imaginacolombia.com>
2024-03-22 13:16:13 -0500

    Fixed: change logic to export

--- Config/config.php
+++ Config/config.php
@@ -2,4 +2,32 @@
 
 return [
   'name' => 'Icommercepricelist',
+  /*
+   |--------------------------------------------------------------------------
+   | Configuration by google sheet template id
+   |--------------------------------------------------------------------------
+   */
+  'synchronizable' => [
+    'entities' => [
+      'icommercepricelist_syncProductlist' => [
+        'base_template_id' => '1OOyW4ySI7RJMdS9fgcJWOfnPpi41FRFq2WmMhcOQonE',
+        'apiRoute' => '/icommercepricelist/v3/product-lists',
+        "supportedActions" =>  ["import", "export"],
+        'sheetName' => 'Icommerce ProductList',
+        'customColumns' => true,
+        'dependencies' => [
+          'icommercepricelist_syncPricelists' => [
+            'apiRoute' => '/icommercepricelist/v3/price-lists',
+            'sheetName' => 'Icommerce PriceList',
+            'columns' => [
+              'id' => 'ID',
+              'name' => 'NOMBRE',
+              'operationPrefix' => "OPERACION",
+              'value' => 'VALOR'
+            ]
+          ],
+        ]
+      ],
+    ]
+  ],
 ];
